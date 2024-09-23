6262287471070074bbedd9b11c5de9b46851262a
YanLob <yanlobato84@gmail.com>
2024-09-22 21:32:06 -0300

    entendimento do HeapSort

--- src/EstruturaDeDados/HeapSort/Main.java
+++ src/EstruturaDeDados/HeapSort/Main.java
@@ -12,8 +12,13 @@ public class Main {
             System.out.print(vetor[i] + " ");
         }
 
+        /*
+        o 'i' nao precisa passar pelo vetor inteiro para que o vetor seja
+        totalmente percorrido, apenas com a metade do vetor - 1, a leitura dele inteiro [e bem sucedida.
+        */
+        // entendido
         int n = vetor.length;
-        for(int i = (n / 2 - 1); i >= 0; i--){ /* nao entendi essa linha de codigo */
+        for(int i = (n / 2 - 1); i >= 0; i--){
             aplicarHeap( vetor, n , i);
         }
 
@@ -25,7 +30,12 @@ public class Main {
 
         for (int i = n - 1; i > 0; i--){
             
-            /** entendi a troca, porem, nao entendi o PQ trocar pela posicao 0 */
+            /* entendi
+            *
+            * o aux vai pra posicao do vetor[0] (comeco), o vetor[0] vai para a posicao do i que [e a ultima e o vetor[i]
+            * vai para o aux que esta no comeco, trocando a posicao dos valores
+            *
+            * */
             int aux = vetor[0];
             vetor[0] = i;
             vetor[i] = aux;
@@ -58,6 +68,9 @@ public class Main {
             raiz = direita;
         }
 
+        // A raiz anda primeiro que o i, se os dois tiverem em posicoes diferentes, quer dizer que o algoritimo esta andando
+        // e quer dizer que deve ter os valores trocados
+
         if(raiz != i){
             int aux = vetor[i];
             vetor[i] = vetor[raiz];
@@ -72,8 +85,8 @@ public class Main {
     /*
     *
     *   blocos para entender 7
-    *   Entendidos => 5
-    *   Nao entendidos => 2
+    *   Entendidos => 7
+    *   Nao entendidos => 0
     *
     * */
 
